0fc08908fe54062ae9b5bc8ccba1efd6a35a9d4b
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-27 10:30:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const promises = [];
+
+for (let i = 0; i < 5; i++) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNum = Math.random();
+      if (randomNum < 0.5) {
+        reject(`Promise ${i+1} rejected with error`);
+      } else {
+        resolve(Math.floor(Math.random() * 10) + 1);
+      }
+    }, Math.random() * 5000); // random delay between 0 and 5 seconds
+  });
+  promises.push(promise);
+}
+
+Promise.all(promises)
+  .then((results) => {
+    const outputDiv = document.getElementById('output');
+    results.forEach((result, i) => {
+      const p = document.createElement('p');
+      p.innerText = `${i+1}: ${result}`;
+      outputDiv.appendChild(p);
+    });
+  })
+  .catch((error) => console.log(error));
